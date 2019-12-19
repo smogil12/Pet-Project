40788c13bab5b8c734bfd48a6ea44d6859c2ae4f
Ben Florentino <55708741+florentinob@users.noreply.github.com>
2019-12-19 10:04:49 -0500

    Donation firebase petproject (#26)
    * Added Collaborator Info
    
    * feat: Initial Homepage HTML
    
    * Updated
    
    * Updated
    
    * doc: Added from to shelters HTML. Added styling
    
    * feat: donation form / firebase JAVA

---
+++ donation.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    var database = firebase.database();
+
+    var initialDonation = 0;
+    var initialDonator = "Be The First To Donate!";
+
+    var recentDonation = initialDonation;
+    var recentDonator = initialDonator;
+
+    database.ref().on("value", function(snapshot) {
+        if (snapshot.child("recentDonator").exist() && snapshot.child("recentDonation").exists()) {
+            recentDonator = snapshot.val().recentDonator;
+            recentDonation = parseInt(snapshot.val().highPrice);
+            $("mostRecentDonator").html(snapshot.val().recentDonator);
+            $("mostRecentDonation").html("$" + snapshot.val().recentDonation);
+
+            console.log(snapshot.val().recentDonator);
+            console.log(snapshot.val().recentDonation);
+        }
+        else {
+            $("#mostRecentDonator").html(recentDonator);
+            $("#mostRecentDonation").html("$" + recentDonation);
+        }
+    })
+
+    $("#button").on("click", function() {
+        var donatorName = $("#donatorName").val().trim();
+        var donationAmount = $("#donationAmount").val().trim();
+
+        console.log(donatorName);
+        console.log(donationAmount);
+
+        if (donationAmount > recentDonation) {
+            alert("Thank you for your donation!");
+            database.ref().set({
+                recentDonator: donatorName,
+                recentDonation: donationAmount
+            })
+        } return false;
+    })
+});
